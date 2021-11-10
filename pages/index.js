eb6e9c804b98d0926fccf1f4cc226c36f2520dee
GioSDA <gsdambrosio@gmail.com>
2021-11-09 16:42:18 -0800

    a

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-import Header from '../components/header'
+import Header from '../components/Header'
 import Nav from '../components/Nav'
 import Results from '../components/Results'
 import requests from '../utils/requests'
